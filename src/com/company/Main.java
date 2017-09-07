21fefcab389072eee308345fc0072d4a67baf8fa
Bruno Klein <brunoklein99@gmail.com>
2017-09-07 13:21:52 -0300

    huffman -> deflate

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.io.*;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        String inputFileHuffman = "C:\\Users\\klein-desk\\IdeaProjects\\informacaoga\\src\\com\\company\\alice29.txt";
+        String outputFileHuffman = "C:\\Users\\klein-desk\\IdeaProjects\\informacaoga\\src\\com\\company\\alice29.huffman";
+        String outputDeflate = "C:\\Users\\klein-desk\\IdeaProjects\\informacaoga\\src\\com\\company\\alice29.huffman.deflate";
+
+        InputStream inHuffman = new BufferedInputStream(new FileInputStream(inputFileHuffman));
+        OutputStream outHuffman = new BufferedOutputStream(new FileOutputStream(outputFileHuffman));
+
+        Huffman.compress(inHuffman, outHuffman);
+
+        InputStream inDeflate = new BufferedInputStream(new FileInputStream(outputFileHuffman));
+        OutputStream outDeflate = new BufferedOutputStream(new FileOutputStream(outputDeflate));
+        outDeflate = new DeflaterOutputStream(outDeflate);
+        copy(inDeflate, outDeflate);
+
+        System.out.println("fim");
+    }
+
+    public static void copy(InputStream is, OutputStream os) throws Exception {
+        byte[] bytes = new byte[1024];
+        int length;
+        try {
+            while ((length = is.read(bytes)) >= 0) {
+                os.write(bytes, 0, length);
+            }
+        } finally {
+            os.close();
+            is.close();
+        }
     }
 }
---
+++ src/com/company/Queue.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by klein-desk on 07-Sep-17.
+ */
+public class Queue {
+}
